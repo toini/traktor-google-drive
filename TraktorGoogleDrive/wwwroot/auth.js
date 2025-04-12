4a1f12aaadc5cc5a03ed94e1d56713f1eb384b27
Toni Juvani <toni@tibber.com>
2025-04-13 01:34:03 +0300

    Add players

---
+++ TraktorGoogleDrive/wwwroot/auth.js
@@ -0,0 +1,18 @@
+﻿window.googleLogin = () => {
+    if (!window.tokenClient) {
+        window.tokenClient = google.accounts.oauth2.initTokenClient({
+            client_id: '219145501841-n6pki0jbvkue0u3vusmnguld6m4fugp9.apps.googleusercontent.com',
+            scope: 'https://www.googleapis.com/auth/drive.readonly',
+            callback: (tokenResponse) => {
+                if (tokenResponse && tokenResponse.access_token) {
+                    sessionStorage.setItem('access_token', tokenResponse.access_token);
+                    console.log('Access token acquired');
+                } else {
+                    console.error('Token response invalid:', tokenResponse);
+                }
+            }
+        });
+    }
+
+    window.tokenClient.requestAccessToken();
+};
