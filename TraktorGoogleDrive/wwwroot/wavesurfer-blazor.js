76afdecb351afe1d633ff75b2e362471230b7ab5
Toni Juvani <toni@tibber.com>
2025-07-17 22:44:44 +0300

    Optional peaks url

--- TraktorGoogleDrive/wwwroot/wavesurfer-blazor.js
+++ TraktorGoogleDrive/wwwroot/wavesurfer-blazor.js
@@ -1,19 +1,33 @@
-window.initWaveSurfer = async function (containerId, wavUrl, peaksUrl, duration) {
+window.initWaveSurfer = async function (containerId, wavUrl, duration, peaksUrl) {
+    console.log('Init WaveSurfer:', containerId, wavUrl, duration, peaksUrl);
+
     if (!window.WaveSurfer) {
-        await import('https://unpkg.com/wavesurfer.js@7/dist/wavesurfer.esm.js');
+        const mod = await import('https://unpkg.com/wavesurfer.js@7/dist/wavesurfer.esm.js');
+        window.WaveSurfer = mod.default || mod.WaveSurfer;
     }
-    const peaksRes = await fetch(peaksUrl);
-    const flatPeaks = await peaksRes.json();
-    const waveform = WaveSurfer.create({
+    let peaks = undefined;
+    if (peaksUrl) {
+        const peaksRes = await fetch(peaksUrl);
+        peaks = [await peaksRes.json()];
+    } else {
+        // Dummy peaks: flat waveform
+        peaks = [Array(Math.max(128, Math.floor(duration))).fill(0)];
+    }
+    const config = {
         container: `#${containerId}`,
         waveColor: 'lightgray',
         progressColor: 'orange',
         height: 128,
         barWidth: 1,
         duration,
-        url: wavUrl,
-        peaks: [flatPeaks],
-    });
+        url: wavUrl
+    };
+    if (peaks) {
+        config.peaks = peaks;
+    }
+    console.log('Creating WaveSurfer instance with config:', config);
+    const waveform = WaveSurfer.create(config);
+    console.log('WaveSurfer instance created:', waveform);
     return waveform;
 };
 window.waveSurferPlayPause = function (waveform) {
